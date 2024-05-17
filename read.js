70b45a2ab13e5ec75cd5c4cb23a48c9601c1ebec
Dan Walker <daniel.walker@gmail.com>
2024-05-17 10:03:04 +1000

    added prettier

--- read.js
+++ read.js
@@ -1,64 +1,66 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs/yargs';
-import {GetParametersByPathCommand, SSMClient} from '@aws-sdk/client-ssm'
+import yargs from "yargs/yargs";
+import { GetParametersByPathCommand, SSMClient } from "@aws-sdk/client-ssm";
 
 const argv = yargs(process.argv.slice(2))
-    .options({
-        p: {
-            type: 'string',
-            alias: 'path',
-            description: 'The SSM path to read parameters from',
-            default: '/'
-        },
-        e: {
-            type: 'boolean',
-            alias: 'dotEnv',
-            description: 'Output plain .env properties',
-            default: false,
-        }
-    })
-    .parseSync();
+  .options({
+    p: {
+      type: "string",
+      alias: "path",
+      description: "The SSM path to read parameters from",
+      default: "/",
+    },
+    e: {
+      type: "boolean",
+      alias: "dotEnv",
+      description: "Output plain .env properties",
+      default: false,
+    },
+  })
+  .parseSync();
 
-const {p: path, e: dotEnv} = argv;
+const { p: path, e: dotEnv } = argv;
 
 const ssm = new SSMClient();
 
 (async () => {
-    const parameters = await readPage(path, [])
+  const parameters = await readPage(path, []);
 
-    if (dotEnv) {
-        parameters.forEach((parameter) => {
-            console.log(`${parameter.Name.split('/').reverse()[0]}=${parameter.Value}`)
-        })
-    } else {
+  if (dotEnv) {
+    parameters.forEach((parameter) => {
+      console.log(
+        `${parameter.Name.split("/").reverse()[0]}=${parameter.Value}`,
+      );
+    });
+  } else {
+    const json = parameters.map((parameter) => {
+      return {
+        Type: parameter.Type,
+        Name: parameter.Name,
+        Value: parameter.Value,
+      };
+    });
 
-        const json = parameters.map((parameter) => {
-            return {
-                Type: parameter.Type,
-                Name: parameter.Name,
-                Value: parameter.Value
-            }
-        })
-
-        console.log(JSON.stringify(json, null, 2))
-    }
+    console.log(JSON.stringify(json, null, 2));
+  }
 })();
 
 async function readPage(path, results, nextToken) {
-    const r = await ssm.send(
-        new GetParametersByPathCommand({
-            Path: path,
-            NextToken: nextToken,
-            Recursive: true,
-            WithDecryption: true,
-        }));
+  const r = await ssm.send(
+    new GetParametersByPathCommand({
+      Path: path,
+      NextToken: nextToken,
+      Recursive: true,
+      WithDecryption: true,
+    }),
+  );
 
-    const newResults = [...results, ...r.Parameters];
+  const newResults = [...results, ...r.Parameters];
 
-    if (r.NextToken) {
-        return readPage(path, newResults, r.NextToken);
-    }
+  if (r.NextToken) {
+    return readPage(path, newResults, r.NextToken);
+  }
 
-    return newResults;
+  return newResults;
 }
--- write.js
+++ write.js
@@ -1,101 +1,102 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs/yargs';
-import fs from 'fs'
-import pThrottle from 'p-throttle';
-import {PutParameterCommand, SSMClient} from '@aws-sdk/client-ssm'
+import yargs from "yargs/yargs";
+import fs from "fs";
+import pThrottle from "p-throttle";
+import { PutParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
 
 const argv = yargs(process.argv.slice(2))
-    .options({
-        p: {
-            type: 'string',
-            alias: 'path',
-            description: 'Prepends the path to all Parameters. Useful for writing .env files'
-        },
-        f: {
-            type: 'string',
-            alias: 'file',
-            description: 'The file to read. ',
-            demandOption: true,
-        },
-        e: {
-            type: 'boolean',
-            alias: 'dotEnv',
-            description: 'Does the file contain plain .env properties',
-            default: false,
-        },
-        o: {
-            type: 'boolean',
-            alias: 'overwrite',
-            description: 'Overwrite the Parameter if it exists',
-            default: false
-        }
-    })
-    .parseSync();
+  .options({
+    p: {
+      type: "string",
+      alias: "path",
+      description:
+        "Prepends the path to all Parameters. Useful for writing .env files",
+    },
+    f: {
+      type: "string",
+      alias: "file",
+      description: "The file to read. ",
+      demandOption: true,
+    },
+    e: {
+      type: "boolean",
+      alias: "dotEnv",
+      description: "Does the file contain plain .env properties",
+      default: false,
+    },
+    o: {
+      type: "boolean",
+      alias: "overwrite",
+      description: "Overwrite the Parameter if it exists",
+      default: false,
+    },
+  })
+  .parseSync();
 
-const {f: file, p: path, o: overwrite, e: dotEnv} = argv;
+const { f: file, p: path, o: overwrite, e: dotEnv } = argv;
 
 const ssm = new SSMClient();
 
-if (path && !path.endsWith('/')) {
-    console.log(`Error: Path must end in /.`)
-    process.exit(1)
+if (path && !path.endsWith("/")) {
+  console.log(`Error: Path must end in /.`);
+  process.exit(1);
 }
 
 // A 2 per second p-throttle.
 const throttle = pThrottle({
-    limit: 2,
-    interval: 1000
+  limit: 2,
+  interval: 1000,
 });
 
 (async () => {
+  if (!fs.existsSync(file)) {
+    console.log(`Error: ${file} does not exist.`);
+    process.exit(1);
+  }
 
-    if (!fs.existsSync(file)) {
-        console.log(`Error: ${file} does not exist.`)
-        process.exit(1)
-    }
+  const items = readFile(file);
 
-    const items = readFile(file);
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
 
-    for (let i = 0; i < items.length; i++) {
+    // There is a rate limit at AWS.
+    const throttled = throttle(() => {
+      return ssm.send(
+        new PutParameterCommand({
+          ...item,
+          Overwrite: overwrite,
+        }),
+      );
+    });
 
-        const item = items[i];
+    // Call the throttled function.
+    await throttled();
 
-        // There is a rate limit at AWS.
-        const throttled = throttle(() => {
-            return ssm.send(new PutParameterCommand({
-                ...item,
-                Overwrite: overwrite
-            }))
-        })
-
-        // Call the throttled function.
-        await throttled();
-
-        console.log(`Written ${item.Name} = ${item.Value} (${item.Type})`);
-    }
+    console.log(`Written ${item.Name} = ${item.Value} (${item.Type})`);
+  }
 })();
 
 function readFile() {
-    const content = fs.readFileSync(file).toString()
+  const content = fs.readFileSync(file).toString();
 
-    // If not a .env then we parse as json and assume it's the output from aws-ssm-read.
-    if (!dotEnv) {
-        return JSON.parse(content);
-    }
+  // If not a .env then we parse as json and assume it's the output from aws-ssm-read.
+  if (!dotEnv) {
+    return JSON.parse(content);
+  }
 
-    // Read as a properties file
-    const lines = content.split('\n')
-    return lines.map((line) => {
-        const [k, v] = line.split('=');
-        const name = `${path ?? ''}${k.trim()}`;
-        const value = v.trim();
+  // Read as a properties file
+  const lines = content.split("\n");
+  return lines.map((line) => {
+    const [k, v] = line.split("=");
+    const name = `${path ?? ""}${k.trim()}`;
+    const value = v.trim();
 
-        // If Parsing as properties we must assume the type is String.
-        return {
-            Name: name,
-            Value: value,
-            Type: 'String'
-        }
-    })
+    // If Parsing as properties we must assume the type is String.
+    return {
+      Name: name,
+      Value: value,
+      Type: "String",
+    };
+  });
 }
